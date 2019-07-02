be305e926ba09f2eedd0e82cdbba99bccb471097
Alline Marjorie <allinemarjorie@gmail.com>
2019-07-02 01:38:05 -0300

    Converte dados do produto para JSON

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 axios.get("https://www.americanas.com.br/produto/134255922/smart-tv-led-40-android-tcl-40s6500-full-hd-com-conversor-digital-wi-fi-bluetooth-1-usb-2-hdmi-controle-remoto-com-comando-de-voz-google-assistant?chave=prf_hm_0_oh_1_txar_00&hl=lower&pfm_carac=1&pfm_index=0&pfm_page=home&pfm_pos=maintop4&pfm_type=vit_spacey").then(function (response) {
 	const $ = cheerio.load(response.data)
@@ -13,44 +13,46 @@ axios.get("https://www.americanas.com.br/produto/134255922/smart-tv-led-40-andro
 		price: extraiPreco($)
 	}
 
-	console.log(dadosProduto)
+	const jsonDadosProduto = JSON.stringify(dadosProduto)
+
+	console.log(jsonDadosProduto)
 })
 
 function extraiId($) {
-	const elementoId = $('#content section section span').first().text();
+	const elementoId = $('#content section section span').first().text()
 
-	let produtoId = elementoId.replace('(Cód.', '').replace(')', '');
-	produtoId = parseInt(produtoId);
-	return produtoId;
+	let produtoId = elementoId.replace('(Cód.', '').replace(')', '')
+	produtoId = parseInt(produtoId)
+	return produtoId
 }
 
 function extraiBreadcrumbs($) {
-	const breadcrumbs = [];
+	const breadcrumbs = []
 
 	$('.product-breadcrumb a').each(function(i, valor) {
 		let crumb = $(valor).text()
 		breadcrumbs.push(crumb)
-	});
+	})
 
 	return breadcrumbs
 }
 
 function extraiNome($) {
-	return $('h1').text();
+	return $('h1').text()
 }
 
 function extraiImagem($) {
-	return $('.gallery img').first().attr('src');
+	return $('.gallery img').first().attr('src')
 }
 
 function extraiVendedor($) {
-	return $('.seller-name-container span').first().text();
+	return $('.seller-name-container span').first().text()
 }
 
 function extraiPreco($) {
-	let preco = $('section').first().find('.sales-price').text();
-	preco = preco.replace("R$ ", "").replace(".", "").replace(",", ".");
-	preco = parseFloat(preco);
+	let preco = $('section').first().find('.sales-price').text()
+	preco = preco.replace("R$ ", "").replace(".", "").replace(",", ".")
+	preco = parseFloat(preco)
 
 	return preco
 }
