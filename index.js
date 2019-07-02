663a90872f98d5773546500f3cfad615d0d019e9
Alline Marjorie <allinemarjorie@gmail.com>
2019-07-02 00:09:30 -0300

    Refatora extração do id para uma função separada

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const cheerio = require('cheerio');
 
 axios.get("https://www.americanas.com.br/produto/132248681/iphone-7-32gb-preto-matte-desbloqueado-ios-10-wi-fi-4g-camera-12mp-apple?DCSext.recom=RR_item_page.rr1-ClickCP&nm_origem=rec_item_page.rr1-ClickCP&nm_ranking_rec=2").then(function (response) {
 	const $ = cheerio.load(response.data)
-		
+	
+	const dadosProduto = {
+		id: extraiId($)
+	}
+
+})
+
+function extraiId($) {
 	const elementoId = $('#content section section span').first().text();
 
 	let produtoId = elementoId.replace('(Cód.', '').replace(')', '');
-	produtoId = parseInt(elementoId);
-	console.log(produtoId);
+	produtoId = parseInt(produtoId);
+	return produtoId;
+}
 
-})
 
