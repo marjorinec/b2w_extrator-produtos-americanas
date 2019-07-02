201df78a187a815b02f2f5f0f9b244c3e6b572e1
Alline Marjorie <allinemarjorie@gmail.com>
2019-07-02 02:01:29 -0300

    Recebe URL do produto como parâmetro

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-function pegaProduto() {
-	axios.get("https://www.americanas.com.br/produto/134255922/smart-tv-led-40-android-tcl-40s6500-full-hd-com-conversor-digital-wi-fi-bluetooth-1-usb-2-hdmi-controle-remoto-com-comando-de-voz-google-assistant?chave=prf_hm_0_oh_1_txar_00&hl=lower&pfm_carac=1&pfm_index=0&pfm_page=home&pfm_pos=maintop4&pfm_type=vit_spacey").then(function (response) {
+function pegaProduto(urlProduto) {
+	axios.get(urlProduto).then(function (response) {
 		const $ = cheerio.load(response.data)
 
 		const dadosProduto = {
